0b764c34f7803ba0ab7ead1b003fa93e6545f964
Viacheslav Gurov <gurov>
2021-01-24 20:13:41 -0500

    dt 24 jan 2021

--- Count_symbols_in_string_hw_2_dt_16_Dec_2019.py
+++ Count_symbols_in_string_hw_2_dt_16_Dec_2019.py
@@ -1,26 +1,26 @@
 # 1.Посчитайте, сколько раз символ встречается в строке.
 # Строка и символ вводятся с клавиатуры.
 
-#1 : Naive method
-string = str(input("Enter the string: ")).lower()
-symbol_to_count = str(input("Enter the symbol to count: ")).lower()
-
-symbols_in_string = len(string)
-
-count = 0
-for i in string:
-    if i == symbol_to_count:
-        count = count + 1
-
-percent_symbol = round(count/symbols_in_string*100, 2)
-
-print(f'Symbols in the string: {symbols_in_string} total. '
-      f'\nSymbol "{symbol_to_count}" repeated in the string: {count} times. '
-      f'\nOr is : {percent_symbol}%.')
+# #1 : Naive method
+# string = str(input("Enter the string: ")).lower()
+# symbol_to_count = str(input("Enter the symbol to count: ")).lower()
+#
+# symbols_in_string = len(string)
+#
+# count = 0
+# for i in string:
+#     if i == symbol_to_count:
+#         count = count + 1
+#
+# percent_symbol = round(count/symbols_in_string*100, 2)
+#
+# print(f'Symbols in the string: {symbols_in_string} total. '
+#       f'\nSymbol "{symbol_to_count}" repeated in the string: {count} times. '
+#       f'\nOr is : {percent_symbol}%.')
 
 ##########
 
-#2 : Using count()
+# # 2 : Using count()
 # test_str = str(input("Enter the string: ")).lower()
 # symbol_to_count = str(input("Enter the symbol to count: ")).lower()
 #
@@ -37,7 +37,7 @@ print(f'Symbols in the string: {symbols_in_string} total. '
 
 ##########
 
-#3 : Using collections.Counter()
+# #3 : Using collections.Counter()
 # from collections import Counter
 # string = str(input("Enter the string: ")).lower()
 # symbol = str(input("Enter the symbol: ")).lower()
@@ -50,7 +50,7 @@ print(f'Symbols in the string: {symbols_in_string} total. '
 
 ##########
 
-#4 : Using lambda + sum() + map()
+# #4 : Using lambda + sum() + map()
 # test_str = str(input("Enter the string: ")).lower()
 # symbol = str(input("Enter the symbol: ")).lower()
 #
@@ -61,12 +61,12 @@ print(f'Symbols in the string: {symbols_in_string} total. '
 #       f'\nOr "{percent}" percent.')
 
 #5 : Using re + findall()
-# import re
-# test_str = str(input("Enter the string: ")).lower()
-# e = str(input("Entet the symbol: ")).lower()
-#
-#
-# count = len(re.findall("e", test_str))
-#
-# print(f'Symbol "{e}" in "{test_str}" repeated "{count}" times. '
-#       f'\nOr "{count/len(test_str)*100}" percent.')
+import re
+test_str = str(input("Enter the string: ")).lower()
+e = str(input("Entet the symbol: ")).lower()
+
+
+count = len(re.findall("e", test_str))
+
+print(f'Symbol "{e}" in "{test_str}" repeated "{count}" times. '
+      f'\nOr "{count/len(test_str)*100}" percent.')
