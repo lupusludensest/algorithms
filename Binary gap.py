f7f9f888c7a4b3cc6df91fc0a795854cefc77171
LupusLudensEst <gurovvic@gmail.com>
2022-06-10 14:42:02 -0400

    dt 10 june 2022

--- Binary gap.py
+++ Binary gap.py
@@ -8,32 +8,31 @@
 # Максимальный разрыв 4 нуля, идущих подряд
 
 n = int(input(f'Введите число: '))
-decimal = n
-# Получаем число в двоичном представлении
-bin_string = ''
-while n > 0:
-    bin_string += str(n % 2)
-    n = n // 2
-bin_string = bin_string[::-1]
-# print(f'Decimal: {decimal} = Binary: {bin_string}')
-#
-# Проверяем что разрыв есть и находим его длину
-max_gap = 0
-counter = 0
-bin_gap = False
-for char in bin_string:
-    if char == '1':
-        if max_gap < counter:
-            max_gap = counter
-        counter = 0
-        bin_gap = True
-    elif bin_gap:
-        counter += 1
-print(f'Decimal: {decimal} = Binary: {bin_string}, max gap is: {max_gap}')
+# decimal = n
+# # Получаем число в двоичном представлении
+# bin_string = ''
+# while n > 0:
+#     bin_string += str(n % 2)
+#     n = n // 2
+# bin_string = bin_string[::-1]
+# # print(f'Decimal: {decimal} = Binary: {bin_string}')
+# #
+# # Проверяем что разрыв есть и находим его длину
+# max_gap = 0
+# counter = 0
+# bin_gap = False
+# for char in bin_string:
+#     if char == '1':
+#         if max_gap < counter:
+#             max_gap = counter
+#         counter = 0
+#         bin_gap = True
+#     elif bin_gap:
+#         counter += 1
+# print(f'Decimal: {decimal} = Binary: {bin_string}, max gap is: {max_gap}')
 
 # вариант для читеров
 bin_str = str(bin(n))
-print(f'{str(bin(n))}')
 # сложность O(N)
 max_gap = 0
 counter = 0
---
+++ practice.py
@@ -0,0 +1,9 @@
+# Disney code interview dt 10 june 2022
+nums = [2,7,11,15]
+def wrkwlst(nums):
+    nlst=[]
+    for e[i] in range(nums[len(nums)-1]):
+        if (e[i] + e[i + 1]) == 9:
+            nlst.append(e[i])
+    return nlst
+print(wrkwlst(nums))
