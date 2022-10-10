203d29cacb060a0baba7109adf62440c199d2733
LupusLudensEst <gurovvic@gmail.com>
2022-10-10 17:27:35 -0500

    10_oct_2022_mon

--- practice.py
+++ practice.py
@@ -1865,6 +1865,15 @@ import pandas as pd
 #     if prime > 1 and prime % i:
 #         print(prime)
 
+# prime number
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n//x)
+#             break
+#     else:
+#         # loop fell through without finding a factor
+#         print(n, 'is a prime number')
 
 
 
