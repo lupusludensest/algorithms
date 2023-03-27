0fc63bd2edc2caf6f113b627a5237f1069ec1dce
LupusLudensEst <gurovvic@gmail.com>
2023-03-26 22:25:20 -0500

    dt 26 mar 2023

--- api_get.py
+++ api_get.py
@@ -3,10 +3,10 @@ import requests
 r=requests.get('https://www.cnn.com/2022/05/27/business/elon-musk-billionaires-tweet/index.html')
 
 if 300 >= r.status_code >= 200:
-    print(f"\nPASS_STATUS = {r.status_code}")
+    print(f"PASS_STATUS = {r.status_code}")
 else:
     print(f'\nFAIL_STATUS ="{r.status_code}"')
-print(f'\nReceived responce = "{r}"')
+
 whole_txt = r.text
 wrd = 'war'
 # print(whole_txt)
