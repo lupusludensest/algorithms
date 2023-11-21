c83a846390465c4391b3f7bdd2727b916512e468
Vic Gurov <gurovvic@gmail.com>
2023-11-21 12:36:18 -0600

    dt 21 nov 2023

--- code_challenge_ CodeSignal_PayPay_Corpdt_21_nov_2023.py
+++ code_challenge_ CodeSignal_PayPay_Corpdt_21_nov_2023.py
@@ -10,10 +10,10 @@ def solution(n):
 
     return int(str_a) + int(str_b)
 
-print(solution(22))
+print(solution(21))
 
 # Quiz. What is the next number in the following sequence?
-1, 1, 2, 3, 5, 8, 13, *
+# 1, 1, 2, 3, 5, 8, 13, *
 # A: 21
 
 # What happens when user asks the browser
@@ -23,9 +23,9 @@ print(solution(22))
 # 3. Request heats loadbalancer of the site;
 # 4. HTTP/S request procesed by the web server;
 # 5. Responce (200 if success) and data transfer (HTML returned to the browser);
-# 5. Page rendering in the browser;
-# 6. JavaScript execution;
-# 7. Page displayed in the browser;
-# 8. User interacts with a dynamic page and JavaScript updates the page;
+# 6. Page rendering in the browser;
+# 7. JavaScript execution;
+# 8. Page displayed in the browser;
+# 9. User interacts with a dynamic page and JavaScript updates the page;
 
 
