7072e93d394f059f76ba8c7cd072fc71b66f2b52
Vic Gurov <35234300+lupusludensest@users.noreply.github.com>
2024-03-08 07:54:26 -0500

    Update fibonaccy_recursion_dt_02_nov_2023.py

--- fibonaccy_recursion_dt_02_nov_2023.py
+++ fibonaccy_recursion_dt_02_nov_2023.py
@@ -1,4 +1,7 @@
 # fibonaccy recursion
+# 0 1 2 3 4 5 6  7  8  9  10
+# 0 1 1 2 3 5 8 13 21 34  55
+
 def fb_rc(n = int(input('Enter the n for fibonaccy recursion: '))):
     if n < 0:
         return f'Error'
